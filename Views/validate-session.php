a8c3b2b068e1c4d7b544c6c2e6689383c806a2e9
giselacruz <62399884+giselacruz@users.noreply.github.com>
2020-10-29 18:57:46 -0300

    modify nav

--- Views/validate-session.php
+++ Views/validate-session.php
@@ -1,6 +1,7 @@
 <?php
  
-  if(!isset($_SESSION["loginUser"])){
-    require_once(FRONT_ROOT."Home/MostrarHome");
+  if(!isset($_SESSION["loginUser"]) ){
+    /*require_once(FRONT_ROOT."Home/MostrarHome");*/
+    header("location:../index.php");  
   }
 ?>
