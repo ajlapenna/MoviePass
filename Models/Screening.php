b728911eb63c1069805ed54986579b1c700f24cf
ajlapenna <agustinjlapenna@gmail.com>
2020-10-30 20:47:34 -0300

    fix

--- Models/Screening.php
+++ Models/Screening.php
@@ -20,45 +20,21 @@
             $this->id_cinema_screening = $id_cinema_screening;
         }
 
-        public function getId_screening()
-        {
-                return $this->id_screening;
-        }
+        public function getId_screening() { return $this->id_screening; }
     
-        public function setId_screening($id_screening)
-        {
-                $this->id_screening = $id_screening;
-        }
+        public function setId_screening($id_screening) { $this->id_screening = $id_screening; }
 
-        public function getDate_screening()
-        {
-                return $this->date_screening;
-        }
+        public function getDate_screening() { return $this->date_screening; }
 
-        public function setDate_screening($date_screening)
-        {
-                $this->date_screening = $date_screening;
-        }
+        public function setDate_screening($date_screening) { $this->date_screening = $date_screening; }
 
-        public function getId_movie_screening()
-        {
-                return $this->id_movie_screening;
-        }
+        public function getId_movie_screening() { return $this->id_movie_screening; }
 
-        public function setId_movie_screening($id_movie_screening)
-        {
-                $this->id_movie_screening = $id_movie_screening;
-        }
+        public function setId_movie_screening($id_movie_screening) { $this->id_movie_screening = $id_movie_screening; }
 
-        public function getId_cinema_screening()
-        {
-                return $this->id_cinema_screening;
-        }
+        public function getId_cinema_screening() { return $this->id_cinema_screening; }
 
-        public function setId_cinema_screening($id_cinema_screening)
-        {
-                $this->id_cinema_screening = $id_cinema_screening;
-        }
+        public function setId_cinema_screening($id_cinema_screening) { $this->id_cinema_screening = $id_cinema_screening; }
     }
 
 
